8db6415f598b27b8c048f7a5662752b2ef944a1d
Neo Yin <yinzhang@berkeley.edu>
2024-07-31 14:30:30 -0700

    custom exceptions module

--- scripts/misc/remove_in_tmp_slide_dir_column.py
+++ scripts/misc/remove_in_tmp_slide_dir_column.py
@@ -5,7 +5,7 @@ from LLRunner.config import slide_metadata_path
 slide_metadata = pd.read_csv(slide_metadata_path)
 
 # remove the 'tmp_slide_dir' column
-slide_metadata = slide_metadata.drop(columns=["tmp_slide_dir"])
+slide_metadata = slide_metadata.drop(columns=["in_tmp_slide_dir"])
 
 # overwrite the slide metadata
 slide_metadata.to_csv(slide_metadata_path, index=False)
