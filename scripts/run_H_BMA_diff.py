cb46bcf335fbce99f7d13fd9cc409ad1d664c403
Neo Yin <yinzhang@berkeley.edu>
2024-07-31 14:13:05 -0700

    custom exceptions module

--- scripts/run_H_BMA_diff.py
+++ scripts/run_H_BMA_diff.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
         delete_slide=True,
         note="Testing the pipeline.",
         hoarding=False,
-        continue_on_error=False,
+        continue_on_error=True,
         do_extract_features=False,
         check_specimen_clf=False,
     )
