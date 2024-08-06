21d195aab373b0064ac4e8cc9aa991f3b2f5836b
Neo Yin <yinzhang@berkeley.edu>
2024-08-06 15:04:40 -0700

    BMAResult module SSH implementation paramiko

--- LLRunner/read/BMAResult.py
+++ LLRunner/read/BMAResult.py
@@ -7,7 +7,15 @@ from pathlib import Path
 from PIL import Image
 from LLRunner.read.read_config import *
 from LLRunner.config import pipeline_run_history_path, slide_metadata_path
-
+import paramiko
+import pandas as pd
+from PIL import Image
+from io import BytesIO
+import os
+import math
+from pathlib import Path
+import random
+import time
 
 def csv_to_dict(file_path):
     result_dict = {}
@@ -507,201 +515,110 @@ class BMAResult:
         pass  # TODO, we need to check the integrity of the stored differential and make sure that it actually matches with the differential computed here.
 
 
-import paramiko
-import pandas as pd
-from PIL import Image
-from io import BytesIO
-import os
-import math
-from pathlib import Path
-import random
-import time
-
 
 class BMAResultSSH:
-    """
-    Class for keeping track of the results of the LLRunner on a BMA slide.
-
-    === Class Attributes ===
-    -- remote_result_dir: the directory where the results are stored
-    -- result_folder_name: the name of the folder where the results are stored
-    -- pipeline: result_folder_name.split("_")[0]
-    -- datetime_processed: result_folder_name.split("_")[1]
-    -- error: a boolean indicating if the result directory is an error directory
-    -- cell_info: a pandas DataFrame containing the cell information
-    -- hostname: the hostname of the server where the results are stored
-    -- username: the username to login to the server
-    -- max_retries: the maximum number of retries for rsync
-    -- backoff_factor: the backoff factor for rsync
-
-    KEY ASSUMPTIONS:
-    -- the file location for metadata files is the same on the server as it is on the local machine
-    """
-
-    def __init__(
-        self,
-        hostname,
-        username,
-        remote_result_dir,
-        max_retries=3,
-        backoff_factor=2,
-    ):
-        """Use the result directory to get the results of the LLRunner on a remote machine."""
-
+    def __init__(self, hostname, username, remote_result_dir, rsa_key_path, max_retries=3, backoff_factor=2):
+        """Initialize the BMAResultSSH class with remote SSH access."""
         self.hostname = hostname
         self.username = username
         self.remote_result_dir = Path(remote_result_dir)
         self.max_retries = max_retries
         self.backoff_factor = backoff_factor
 
-        # Set up the SSH client and SFTP
         self.ssh_client = paramiko.SSHClient()
         self.ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.ssh_client.connect(hostname=self.hostname, username=self.username)
+        self.ssh_client.connect(hostname=self.hostname, username=self.username, key_filename=rsa_key_path)
         self.sftp_client = self.ssh_client.open_sftp()
 
-        # Check if the result directory exists on the remote server
         try:
             self.sftp_client.stat(str(self.remote_result_dir))
         except FileNotFoundError:
-            raise NotADirectoryError(
-                f"Result directory {self.remote_result_dir} does not exist on the remote server."
-            )
+            raise NotADirectoryError(f"Result directory {self.remote_result_dir} does not exist on the remote server.")
 
-        # Get the name of the folder where the results are stored
         self.result_folder_name = self.remote_result_dir.name
-
-        # Get the pipeline from the result_folder_name
         self.pipeline = self.result_folder_name.split("_")[0]
-
-        # Get the datetime_processed from the result_folder_name
         self.datetime_processed = self.result_folder_name.split("_")[1]
-
-        # Second check if the result directory's folder name starts with "ERROR_"
         self.error = self.result_folder_name.startswith("ERROR_")
 
-        # Load cell_info from the remote CSV file
-        with self.sftp_client.open(
-            str(self.remote_result_dir / "cells" / "cells_info.csv"), "r"
-        ) as f:
+        with self.sftp_client.open(str(self.remote_result_dir / "cells" / "cells_info.csv"), "r") as f:
             self.cell_info = pd.read_csv(f)
 
-    def get_stacked_differential(self):
-        """Compute the stacked differential from the cell info."""
-        cell_columns = self.cell_info[cellnames]
-        average_probabilities = cell_columns.mean()
-        return average_probabilities.to_dict()
+    def has_error(self):
+        return self.error
 
-    def get_one_hot_differential(self):
-        """Compute the one-hot differential from the cell info."""
-        cell_columns = self.cell_info[cellnames]
-        predicted_classes = cell_columns.idxmax(axis=1)
-        one_hot_differential = predicted_classes.value_counts(normalize=True)
-        return one_hot_differential.to_dict()
+    def get_region_confidence(self, region_idx):
+        """Get the confidence scores for a specific region."""
+        focus_regions_info_path = self.remote_result_dir / "focus_regions" / "focus_regions_info.csv"
+        high_mag_focus_regions_info_path = self.remote_result_dir / "focus_regions" / "high_mag_focus_regions_info.csv"
 
-    def get_grouped_differential(self):
-        """Compute the grouped differential from the cell info."""
-        self.cell_info[omitted_classes] = 0
-        self.cell_info["cell_class"] = self.cell_info[cellnames].idxmax(axis=1)
-        self.cell_info = self.cell_info[
-            ~self.cell_info["cell_class"].isin(removed_classes)
-        ]
-        self.cell_info["grouped_class"] = self.cell_info["cell_class"].apply(
-            lambda x: next(
-                (
-                    grouped_class
-                    for grouped_class in differential_group_dict
-                    if x in differential_group_dict[grouped_class]
-                ),
-                None,
-            )
-        )
-        grouped_differential = self.cell_info["grouped_class"].value_counts(
-            normalize=True
-        )
-        return grouped_differential.to_dict()
+        with self.sftp_client.open(str(focus_regions_info_path), "r") as f:
+            focus_regions_info = pd.read_csv(f)
 
-    def get_grouped_stacked_differential(self):
-        """Compute the grouped stacked differential from the cell info."""
-        self.cell_info[omitted_classes] = 0
-        self.cell_info["cell_class"] = self.cell_info[cellnames].idxmax(axis=1)
-        self.cell_info = self.cell_info[
-            ~self.cell_info["cell_class"].isin(removed_classes)
-        ]
+        row = focus_regions_info[focus_regions_info["idx"] == int(region_idx)]
 
-        for grouped_class, cellnames_list in differential_group_dict.items():
-            self.cell_info[grouped_class + "_grouped_stacked"] = self.cell_info[
-                cellnames_list
-            ].sum(axis=1)
+        assert len(row) >= 1, f"Region with idx {region_idx} not found in focus_regions_info.csv"
+        assert len(row) == 1, f"Multiple regions with idx {region_idx} found. This should not happen."
 
-        grouped_stacked_differential = self.cell_info[
-            [
-                grouped_class + "_grouped_stacked"
-                for grouped_class in differential_group_dict
-            ]
-        ].mean()
+        low_mag_confidence = row["adequate_confidence_score"].values[0]
 
-        prob_dict = grouped_stacked_differential.to_dict()
-        prob_sum = sum(prob_dict.values())
-        return {key: value / prob_sum for key, value in prob_dict.items()}
+        with self.sftp_client.open(str(high_mag_focus_regions_info_path), "r") as f:
+            high_mag_focus_regions_info = pd.read_csv(f)
 
-    def get_raw_counts(self):
-        """Compute the raw counts of cells from the cell info."""
-        self.cell_info["cell_class"] = self.cell_info[cellnames].idxmax(axis=1)
-        raw_counts = self.cell_info["cell_class"].value_counts()
-        return raw_counts.to_dict()
+        row = high_mag_focus_regions_info[high_mag_focus_regions_info["idx"] == int(region_idx)]
 
-    def get_grouped_raw_counts(self):
-        """Compute the grouped raw counts of cells from the cell info."""
-        self.cell_info[omitted_classes] = 0
-        self.cell_info["cell_class"] = self.cell_info[cellnames].idxmax(axis=1)
-        self.cell_info = self.cell_info[
-            ~self.cell_info["cell_class"].isin(removed_classes)
-        ]
-        self.cell_info["grouped_class"] = self.cell_info["cell_class"].apply(
-            lambda x: next(
-                (
-                    grouped_class
-                    for grouped_class in differential_group_dict
-                    if x in differential_group_dict[grouped_class]
-                ),
-                None,
-            )
-        )
-        grouped_raw_counts = self.cell_info["grouped_class"].value_counts()
-        return grouped_raw_counts.to_dict()
+        if len(row) == 0:
+            return low_mag_confidence, None
 
-    def get_grid_rep(self):
-        """Return the grid rep image of the slide."""
-        grid_rep_path = self.remote_result_dir / "top_view_grid_rep.png"
-        with self.sftp_client.open(str(grid_rep_path), "rb") as f:
-            return Image.open(BytesIO(f.read()))
+        assert len(row) == 1, f"Multiple regions with idx {region_idx} found in high_mag_focus_regions_info.csv. This should not happen."
 
-    def get_confidence_heatmap(self):
-        """Return the confidence heatmap image of the slide."""
-        confidence_heatmap_path = self.remote_result_dir / "confidence_heatmap.png"
-        with self.sftp_client.open(str(confidence_heatmap_path), "rb") as f:
-            return Image.open(BytesIO(f.read()))
+        high_mag_confidence = row["adequate_confidence_score_high_mag"].values[0]
+
+        return low_mag_confidence, high_mag_confidence
+
+    def get_focus_regions(self, num_to_sample=5):
+        """Return images of the top regions of the slide."""
+        high_mag_unannotated_dir = self.remote_result_dir / "focus_regions" / "high_mag_unannotated"
+        high_mag_annotated_dir = self.remote_result_dir / "focus_regions" / "high_mag_annotated"
+
+        focus_regions_files = self.sftp_client.listdir(str(high_mag_unannotated_dir))
+        focus_regions_files = [f for f in focus_regions_files if f.endswith(".jpg")]
+
+        focus_regions_files_sample = random.sample(focus_regions_files, num_to_sample)
+
+        focus_regions_images = []
+        focus_regions_annotated_images = []
+        for file_name in focus_regions_files_sample:
+            with self.sftp_client.open(str(high_mag_unannotated_dir / file_name), "rb") as f:
+                focus_regions_images.append(Image.open(BytesIO(f.read())))
+            with self.sftp_client.open(str(high_mag_annotated_dir / file_name), "rb") as f:
+                focus_regions_annotated_images.append(Image.open(BytesIO(f.read())))
+
+        region_idxs = [Path(file).stem for file in focus_regions_files_sample]
+
+        low_mag_confidences = []
+        high_mag_confidences = []
+
+        for region_idx in region_idxs:
+            low_mag_confidence, high_mag_confidence = self.get_region_confidence(region_idx)
+
+            assert high_mag_confidence is not None, f"High mag confidence for region {region_idx} is None."
+
+            low_mag_confidences.append(low_mag_confidence)
+            high_mag_confidences.append(high_mag_confidence)
+
+        return focus_regions_images, focus_regions_annotated_images, region_idxs, low_mag_confidences, high_mag_confidences
 
     def get_runtime_breakdown(self):
         """Return the runtime breakdown of the slide."""
         runtime_data_path = self.remote_result_dir / "runtime_data.csv"
+
         with self.sftp_client.open(str(runtime_data_path), "r") as f:
             runtime_data_dict = self.csv_to_dict(f)
+
         return runtime_data_dict
 
-    def csv_to_dict(self, file_obj):
-        """Convert a CSV file into a dictionary."""
-        result_dict = {}
-        reader = pd.read_csv(file_obj)
-        for _, row in reader.iterrows():
-            key = row[0]
-            value = float(row[1])
-            result_dict[key] = value
-        return result_dict
-
     def _convert_size(self, size_bytes):
-        """Convert bytes to a more human-readable format."""
+        """Convert bytes to a more human-readable format (KB, MB, GB, etc.)."""
         if size_bytes == 0:
             return "0B"
         size_name = ("B", "KB", "MB", "GB", "TB")
@@ -714,7 +631,7 @@ class BMAResultSSH:
         """Return the storage consumption breakdown of the slide result folder."""
         breakdown = {}
         for attr in self.sftp_client.listdir_attr(str(self.remote_result_dir)):
-            if not attr.filename.startswith("."):
+            if not attr.filename.startswith('.'):
                 extension = os.path.splitext(attr.filename)[1].lower()
                 if extension not in breakdown:
                     breakdown[extension] = 0
@@ -726,30 +643,96 @@ class BMAResultSSH:
 
         return breakdown_readable
 
-    def rsync_files(self, remote_source, local_dest, options="-avz"):
-        """Use rsync to copy files from remote_source to local_dest with retry logic."""
-        for attempt in range(self.max_retries):
-            try:
-                command = f"rsync {options} {self.username}@{self.hostname}:{remote_source} {local_dest}"
-                os.system(command)
-                break  # Exit loop if successful
-            except Exception as e:
-                if attempt < self.max_retries - 1:
-                    wait_time = self.backoff_factor * (2**attempt)
-                    print(
-                        f"Attempt {attempt + 1} failed: {e}. Retrying in {wait_time} seconds..."
-                    )
-                    time.sleep(wait_time)
-                else:
-                    raise RuntimeError(
-                        f"Rsync failed after {self.max_retries} attempts: {e}"
-                    )
+    def get_run_history(self):
+        """Return the run history of the slide."""
+        pipeline_run_history_path = self.remote_result_dir / "pipeline_run_history.csv"
+
+        with self.sftp_client.open(str(pipeline_run_history_path), "r") as f:
+            run_history = pd.read_csv(f)
+
+        row = run_history[
+            (run_history["pipeline"] == self.pipeline)
+            & (run_history["datetime_processed"] == self.datetime_processed)
+        ]
+
+        assert len(row) >= 1, f"Row not found in run_history for {self.pipeline} and {self.datetime_processed}"
+        assert len(row) == 1, f"Multiple rows found in run_history for {self.pipeline} and {self.datetime_processed}. This should not happen."
+
+        run_history_dict = row.to_dict(orient="records")[0]
+
+        return run_history_dict
+
+    def get_wsi_name(self):
+        """Return the wsi_name of the slide."""
+        run_history_dict = self.get_run_history()
+        return run_history_dict["wsi_name"]
+
+    def get_slide_metadata_dict(self):
+        """Return the slide metadata dictionary."""
+        wsi_name = self.get_wsi_name()
+        slide_metadata_path = self.remote_result_dir / "slide_metadata.csv"
+
+        with self.sftp_client.open(str(slide_metadata_path), "r") as f:
+            slide_metadata = pd.read_csv(f)
+
+        row = slide_metadata[slide_metadata["wsi_name"] == wsi_name]
+
+        assert len(row) >= 1, f"Row not found in slide_metadata for {wsi_name}"
+        assert len(row) == 1, f"Multiple rows found in slide_metadata for {wsi_name}. This should not happen."
+
+        slide_metadata_dict = row.to_dict(orient="records")[0]
+
+        return slide_metadata_dict
+
+    def get_datetime_processed(self):
+        return self.datetime_processed
+
+    def get_pipeline(self):
+        return self.pipeline
+
+    def get_part_description(self):
+        """Return the part_description of the slide."""
+        part_desc = self.get_slide_metadata_dict().get("part_description", "Missing part description")
+
+        if pd.isna(part_desc) or part_desc.isspace() or not part_desc:
+            return "Missing part description"
+
+        return part_desc
+
+    def get_Dx_and_sub_Dx(self):
+        """Return the Dx and sub_Dx of the slide."""
+        Dx, sub_Dx = (
+            self.get_slide_metadata_dict().get("Dx", "Missing Dx"),
+            self.get_slide_metadata_dict().get("sub_Dx", "Missing sub_Dx")
+        )
+
+        if pd.isna(Dx) or Dx.isspace() or not Dx:
+            Dx = "Missing Dx"
+
+        if pd.isna(sub_Dx) or sub_Dx.isspace() or not sub_Dx:
+            sub_Dx = "Missing sub_Dx"
+
+        return Dx, sub_Dx
+
+    def get_stored_differential(self):
+        """Placeholder for stored differential logic."""
+        pass  # TODO: Implement the logic to check integrity of stored differential
+
+    def csv_to_dict(self, file_obj):
+        """Convert a CSV file into a dictionary."""
+        result_dict = {}
+        reader = pd.read_csv(file_obj)
+        for _, row in reader.iterrows():
+            key = row[0]
+            value = float(row[1])
+            result_dict[key] = value
+        return result_dict
 
     def __del__(self):
         """Cleanup the SSH and SFTP connections."""
-        if hasattr(self, "sftp_client") and self.sftp_client:
+        if hasattr(self, 'sftp_client') and self.sftp_client:
             self.sftp_client.close()
-        if hasattr(self, "ssh_client") and self.ssh_client:
+        if hasattr(self, 'ssh_client') and self.ssh_client:
             self.ssh_client.close()
 
 
