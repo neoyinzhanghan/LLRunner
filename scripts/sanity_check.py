5c65795415462213cbe47739cd16b9d677c47698
Neo Yin <yinzhang@berkeley.edu>
2024-08-18 13:11:17 -0700

    cells pooling ipynb

--- scripts/sanity_check.py
+++ scripts/sanity_check.py
@@ -3,14 +3,16 @@ from LLRunner.config import results_dir
 
 # find all the directories in results_dir that starts with BMA-diff
 directories = [x for x in os.listdir(results_dir) if x.startswith("BMA-diff")]
-paths = [os.path.join(results_dir, x) for x in directories]
-
 bad_dirs = []
 # for each directory, find the slide name and the specimen type
-# check to see if the paths/cells subdirs exist
+# check to see if the cells subdirs exist
 
-for path in paths:
-    if not os.path.exists(os.path.join(path, "cells")):
-        bad_dirs.append(path)
+for dir_name in directories:
+    path = os.path.join(results_dir, dir_name)
+
+    if not os.path.exists(os.path.join(path, "cells")) and not os.path.exists(
+        os.path.join(path, "error.txt")
+    ):
+        bad_dirs.append(dir_name)
 
 print(bad_dirs)
