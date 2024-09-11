d8a77619501b6fa58584435e4a59d2b8091e8087
Neo Yin <yinzhang@berkeley.edu>
2024-09-11 10:57:32 -0700

    topview specimen clf sanity check script

--- scripts/misc/moving_dzsave_to_isilon.py
+++ scripts/misc/moving_dzsave_to_isilon.py
@@ -1,4 +1,5 @@
 import os
+import time
 import pandas as pd
 from LLRunner.config import dzsave_dir, dzsave_metadata_path
 from tqdm import tqdm
@@ -20,3 +21,33 @@ error_rows = dzsave_metadata_df[
 # print the number of rows with errors
 print(f"Number of rows with errors: {len(error_rows)}")
 
+profile_metadata = {
+    "subdir": [],
+    "glv_zipping_time": [],
+    "rsync_time": [],
+    "isilon_unzipping_time": [],
+}
+
+for subdir in tqdm(subdirs, desc="Profilling dzsave archiving"):
+    starttime = time.time()
+    # zip the directory
+    os.system(f"zip -r {subdir}.zip {subdir}")
+    glv_zipping_time = time.time() - starttime
+
+    starttime = time.time()
+    # rsync the zip file to the isilon
+    os.system(f"rsync -av {subdir}.zip {archive_dir}")  
+    rsync_time = time.time() - starttime
+    
+    # unzip the file on the isilon
+    starttime = time.time()
+    os.system(f"unzip {archive_dir}/{subdir}.zip -d {archive_dir}")
+    isilon_unzipping_time = time.time() - starttime
+
+    profile_metadata["subdir"].append(subdir)
+    profile_metadata["glv_zipping_time"].append(glv_zipping_time)
+    profile_metadata["rsync_time"].append(rsync_time)
+    profile_metadata["isilon_unzipping_time"].append(isilon_unzipping_time)
+
+profile_metadata_df = pd.DataFrame(profile_metadata)
+profile_metadata_df.to_csv(os.path.join(dzsave_dir, "archive_profile_metadata.csv"), index=False)
