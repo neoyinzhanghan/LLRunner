b16597bffa5b7f67d3e10eb2cd2a2be039a0f4a7
Neo Yin <yinzhang@berkeley.edu>
2024-09-12 05:27:49 -0700

    topview specimen clf sanity check script

--- LLRunner/slide_processing/ray_sudo_move.py
+++ LLRunner/slide_processing/ray_sudo_move.py
@@ -7,7 +7,7 @@ import shlex  # For safely escaping file paths
 
 # Define the source and destination directories
 directory_to_copy = "/media/hdd3/neo/dzsave_dir/H24-1514;S15;MSKW - 2024-07-10 23.01.33"  # Directory to move to Isilon
-save_dir = "/dmpisilon_tools/neo/test_archive"  # Directory to move to
+save_dir = "/dmpisilon_tools/neo/test_archive_2"  # Directory to move to
 batch_size = 10  # Adjust based on system capacity and optimal performance
 
 # Ensure the destination directory exists
