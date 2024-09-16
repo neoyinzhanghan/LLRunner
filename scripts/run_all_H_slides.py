a7e336267a484594b9583954cdccc3620ef0805b
Neo Yin <yinzhang@berkeley.edu>
2024-09-16 13:19:11 -0700

    concurrent processing for LLBMA

--- scripts/run_all_H_slides.py
+++ scripts/run_all_H_slides.py
@@ -1,3 +1,4 @@
+from LLRunner.deletion.delete_slide_results import delete_results_from_note
 from LLRunner.slide_processing.concurrent_processing import main_concurrent_processing
 
 
@@ -9,10 +10,14 @@ def identity_filter(pipeline_history_df):
     return pipeline_history_df
 
 
+note = "First all-slide BMA-diff and PBS-diff processing with specimen classification. Begin on 2024-09-16."
+
+delete_results_from_note(note=note, ask_for_confirmation=False)
+
 main_concurrent_processing(
     wsi_name_filter_func=start_with_H_filter,
     processing_filter_func=identity_filter,
     num_rsync_workers=4,
-    note="First all-slide BMA-diff and PBS-diff processing with specimen classification. Begin on 2024-09-16.",
+    note=note,
     delete_slide=True,
 )
