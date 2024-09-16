9284b91088c88a11b6045eeb3833316ed53f76a3
Neo Yin <yinzhang@berkeley.edu>
2024-09-15 22:55:55 -0700

    concurrent processing for LLBMA

--- scripts/misc/benchmark_speed.py
+++ scripts/misc/benchmark_speed.py
@@ -4,6 +4,11 @@ import os
 from tqdm import tqdm
 from PIL import Image
 
+hdd_h5_path = "/media/hdd3/neo/random_data.h5"
+hdd_folder_path = "/media/hdd3/neo/random_images"
+isilon_h5_path = "/dmpisilon_tools/neo/dzsave_test/random_data.h5"
+isilon_folder_path = "/dmpisilon_tools/neo/dzsave_test/random_images"
+
 
 def create_random_h5_file(h5_path, rows, columns, patch_size):
     # initialize the h5 file with a dataset named "data", which is an array of shape (rows, columns, patch_size, patch_size, 3)
@@ -17,8 +22,10 @@ def create_random_h5_file(h5_path, rows, columns, patch_size):
                 data[i, j] = np.random.randint(0, 256, (patch_size, patch_size, 3))
 
 
+starttime = time.time()
 # create a random h5 file with 1000 rows, 1000 columns, and 32x32 patches
-create_random_h5_file("random_data.h5", 400, 400, 32)
+create_random_h5_file(hdd_h5_path, 400, 400, 32)
+h5_creation_time = time.time() - starttime
 
 
 def create_random_image_folder(folder_path, rows, columns, patch_size):
@@ -39,7 +46,9 @@ def create_random_image_folder(folder_path, rows, columns, patch_size):
             Image.fromarray(image).save(image_path)
 
 
-create_random_image_folder("random_images", 400, 400, 32)
+starttime = time.time()
+create_random_image_folder(hdd_folder_path, 400, 400, 32)
+folder_creation_time = time.time() - starttime
 
 # now benchmark how long it takes to load 1000 random images from the h5 file vs the image folder
 import time
@@ -71,7 +80,30 @@ def benchmark_loading_folder(folder_path, nums_to_load):
     return time.time() - start
 
 
-h5_time = benchmark_loading_h5("random_data.h5", 10000)
-folder_time = benchmark_loading_folder("random_images", 10000)
-print(f"Loading from h5 took {h5_time:.2f} seconds")
-print(f"Loading from folder took {folder_time:.2f} seconds")
+h5_local_loading_time = benchmark_loading_h5(hdd_h5_path, 10000)
+folder_local_loading_time = benchmark_loading_folder(hdd_folder_path, 10000)
+
+# sudo rsync -av the h5 file and the image folder to /dmpisilon_tools/neo/dzsave_test
+# and then benchmark how long it takes to load the h5 file vs the image folder from the network
+starttime = time.time()
+os.system(f"sudo rsync -av {hdd_h5_path} {isilon_h5_path}")
+h5_network_copy_time = time.time() - starttime
+
+starttime = time.time()
+os.system(f"sudo rsync -av {hdd_folder_path} {isilon_folder_path}")
+folder_network_copy_time = time.time() - starttime
+
+starttime = time.time()
+h5_network_loading_time = benchmark_loading_h5(isilon_h5_path, 10000)
+folder_network_loading_time = benchmark_loading_folder(isilon_folder_path, 10000)
+
+print(f"Creating h5 file took {h5_creation_time:.2f} seconds")
+print(f"Creating image folder took {folder_creation_time:.2f} seconds")
+print(f"Copying h5 file to network took {h5_network_copy_time:.2f} seconds")
+print(f"Copying image folder to network took {folder_network_copy_time:.2f} seconds")
+print(f"Loading from h5 from hdd took {h5_local_loading_time:.2f} seconds")
+print(f"Loading from folder from hdd took {folder_local_loading_time:.2f} seconds")
+print(f"Loading from h5 from network took {h5_network_loading_time:.2f} seconds")
+print(
+    f"Loading from folder from network took {folder_network_loading_time:.2f} seconds"
+)
