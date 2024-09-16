2af4d63b1495607b0a0b3e0fa0333f98fd54844b
Neo Yin <yinzhang@berkeley.edu>
2024-09-15 22:21:15 -0700

    concurrent processing for LLBMA

--- scripts/misc/benchmark_speed.py
+++ scripts/misc/benchmark_speed.py
@@ -28,13 +28,16 @@ def create_random_image_folder(folder_path, rows, columns, patch_size):
 
     # Create the folder if it does not exist
     os.makedirs(folder_path, exist_ok=True)
-    
+
     # Fill the folder with random images
     for i in tqdm(range(rows), desc="Creating random image folder"):
         for j in tqdm(range(columns), leave=False):
-            image = np.random.randint(0, 256, (patch_size, patch_size, 3), dtype=np.uint8)
+            image = np.random.randint(
+                0, 256, (patch_size, patch_size, 3), dtype=np.uint8
+            )
             image_path = os.path.join(folder_path, f"image_{i}_{j}.png")
             Image.fromarray(image).save(image_path)
+
 
 create_random_image_folder("random_images", 400, 400, 32)
 
@@ -42,25 +45,33 @@ create_random_image_folder("random_images", 400, 400, 32)
 import time
 
 
-def benchmark_loading_h5(h5_path, rows, columns):
+def benchmark_loading_h5(h5_path, nums_to_load):
     start = time.time()
     with h5py.File(h5_path, "r") as h5_file:
-        for i in tqdm(range(rows), desc="Benchmarking loading h5"):
-            for j in tqdm(range(columns), leave=False):
-                image = h5_file["data"][i, j]
+        for i in range(nums_to_load):
+            # randomly sample a row and column
+            row = np.random.randint(0, h5_file["data"].shape[0])
+            column = np.random.randint(0, h5_file["data"].shape[1])
+
+            # load the image
+            image = h5_file["data"][row, column]
     return time.time() - start
 
 
-def benchmark_loading_folder(folder_path, rows, columns):
+def benchmark_loading_folder(folder_path, nums_to_load):
     start = time.time()
-    for i in tqdm(range(rows), desc="Benchmarking loading folder"):
-        for j in tqdm(range(columns), leave=False):
-            image_path = os.path.join(folder_path, f"image_{i}_{j}.png")
-            image = np.array(Image.open(image_path))
+    for i in range(nums_to_load):
+        # randomly sample a row and column
+        row = np.random.randint(0, 400)
+        column = np.random.randint(0, 400)
+
+        # load the image
+        image_path = os.path.join(folder_path, f"image_{row}_{column}.png")
+        image = np.array(Image.open(image_path))
     return time.time() - start
 
 
-h5_time = benchmark_loading_h5("random_data.h5", 400, 400)
-folder_time = benchmark_loading_folder("random_images", 400, 400)
+h5_time = benchmark_loading_h5("random_data.h5", 1000)
+folder_time = benchmark_loading_folder("random_images", 1000)
 print(f"Loading from h5 took {h5_time:.2f} seconds")
 print(f"Loading from folder took {folder_time:.2f} seconds")
