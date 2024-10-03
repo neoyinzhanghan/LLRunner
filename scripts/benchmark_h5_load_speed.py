a4ea3013283c158d8b91c970cfe102c604ce90f4
Neo Yin <yinzhang@berkeley.edu>
2024-10-03 15:38:44 -0700

    finalize h5

--- scripts/benchmark_h5_load_speed.py
+++ scripts/benchmark_h5_load_speed.py
@@ -4,6 +4,7 @@ import openslide
 import numpy as np
 from LLRunner.slide_processing.dzsave_h5 import dzsave_wsi_name_h5, retrieve_tile_h5
 from LLRunner.config import tmp_slide_dir
+from tqdm import tqdm
 
 slide_name = "H19-5749;S10;MSKI - 2023-05-24 21.38.53.ndpi"
 h5_path = "/dmpisilon_tools/neo/dzsave_bm/6825083.h5"
@@ -15,7 +16,7 @@ tile_size = 256
 num_to_retrieve = 1000
 
 start_time = time.time()
-for i in range(num_to_retrieve):
+for i in tqdm(range(num_to_retrieve), desc="Retrieving tiles from h5"):
     # find a random level from 0, 1, ... 18
     random_level = np.random.randint(13, 19)
     downsample_factor = 2 ** (18 - random_level)
