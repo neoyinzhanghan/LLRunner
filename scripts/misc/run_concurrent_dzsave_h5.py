6671a2d9d09142de276528fe9f3ea7905c894387
Neo Yin <yinzhang@berkeley.edu>
2024-10-16 02:14:40 -0700

    h5 visual sanity check script

--- scripts/misc/run_concurrent_dzsave_h5.py
+++ scripts/misc/run_concurrent_dzsave_h5.py
@@ -1,10 +1,6 @@
 import time
-import random
 import pandas as pd
 from LLRunner.slide_processing.concurrent_processing import main_concurrent_dzsave_h5
-
-# set the seed so that random results are reproducible
-random.seed(42)
 
 wsi_tracker_csv_path = "/media/hdd3/neo/test_diff_results.csv"
 
@@ -14,11 +10,8 @@ df = pd.read_csv(wsi_tracker_csv_path)
 # get the wsi_name as a list
 wsi_names = df["wsi_name"].tolist()
 
-# randomly select 10 wsi names
-wsi_names = random.sample(wsi_names, 10)
 
-
-def test_wsi_name_filter(wsi_name: str) -> bool:
+def wsi_name_filter(wsi_name: str) -> bool:
     """
     Simple filter function to filter out the wsi names.
     """
@@ -27,7 +20,7 @@ def test_wsi_name_filter(wsi_name: str) -> bool:
 
 start_time = time.time()
 main_concurrent_dzsave_h5(
-    wsi_name_filter_func=test_wsi_name_filter,
+    wsi_name_filter_func=wsi_name_filter,
     num_rsync_workers=4,
     delete_slide=True,
 )
