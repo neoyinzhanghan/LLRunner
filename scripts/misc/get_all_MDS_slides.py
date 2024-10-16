f77c6e420373d1c054467f28452c8033b3ab55a5
Neo Yin <yinzhang@berkeley.edu>
2024-10-15 23:02:44 -0700

    MDS look up script

--- scripts/misc/get_all_MDS_slides.py
+++ scripts/misc/get_all_MDS_slides.py
@@ -1,4 +1,7 @@
+import os
 import pandas as pd
+from subprocess import call
+from LLBMA.front_end.api import analyse_bma
 from tqdm import tqdm
 from LLRunner.read.SST import sst
 from LLRunner.custom_errors import AccessionNumberNotFoundError
@@ -12,6 +15,7 @@ df = pd.read_csv(processed_slides_path)
 wsi_names = df["wsi_name"].tolist()
 
 MDS_wsi_names_df_dict = {"wsi_name": [], "Dx": [], "sub_Dx": []}
+MDS_wsi_names = []
 
 for wsi_name in tqdm(wsi_names, desc="Finding MDS Slides"):
     accession_number = wsi_name.split(";")[0]
@@ -28,6 +32,8 @@ for wsi_name in tqdm(wsi_names, desc="Finding MDS Slides"):
             MDS_wsi_names_df_dict["Dx"].append(dx)
             MDS_wsi_names_df_dict["sub_Dx"].append(subdx)
 
+            MDS_wsi_names.append(wsi_name)
+
     except AccessionNumberNotFoundError:
         continue
 
@@ -38,3 +44,29 @@ MDS_wsi_names_df = pd.DataFrame(MDS_wsi_names_df_dict)
 
 # save the dataframe to a csv file in media/hdd3/neo
 MDS_wsi_names_df.to_csv("/media/hdd3/neo/MDS_wsi_names.csv", index=False)
+
+slide_save_dir = "/media/hdd3/neo/MDS_slides"
+os.makedirs(slide_save_dir, exist_ok=True)
+
+
+def rsync_slide(slide_path, destination_dir):
+    """Run rsync to sync the dzsave output to the destination."""
+    rsync_command = [
+        "sudo",
+        "rsync",
+        "-a",  # Adjust options based on your needs
+        slide_path,
+        destination_dir,
+    ]
+    result = call(rsync_command)
+    if result == 0:
+        print(f"Successfully synced {slide_path} to {destination_dir}")
+    else:
+        print(f"Failed to sync {slide_path}. Error code: {result}")
+
+
+slide_source_dir = "/pesgisipth/NDPI"
+
+for wsi_name in tqdm(MDS_wsi_names, desc="Syncing MDS Slides"):
+    wsi_path = os.path.join(slide_source_dir, wsi_name)
+    rsync_slide(wsi_path, slide_save_dir)
