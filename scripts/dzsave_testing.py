6229ffe401c3bab7b675d06c148a8eee68b7768b
Neo Yin <yinzhang@berkeley.edu>
2024-12-13 14:49:11 -0800

    dzsave latency test for Cesar

--- scripts/dzsave_testing.py
+++ scripts/dzsave_testing.py
@@ -3,6 +3,7 @@ from LLRunner.slide_processing.dzsave_h5 import dzsave_h5
 
 test_slide_ndpi_path = "/media/hdd3/neo/test_slide_2.ndpi"
 save_dir = "/media/hdd3/neo/test_slide_2_dzsave"
+os.makedirs(save_dir, exist_ok=True)
 h5_path_256 = "test_slide_2_256.h5"
 h5_path_512 = "test_slide_2_512.h5"
 h5_path_1024 = "test_slide_2_1024.h5"
