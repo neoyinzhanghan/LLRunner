54ba981e1be1202d808c6da2c8d40c5a67ef2dc7
Neo Yin <yinzhang@berkeley.edu>
2024-12-13 14:48:50 -0800

    dzsave latency test for Cesar

---
+++ scripts/dzsave_testing.py
@@ -0,0 +1,35 @@
+import os
+from LLRunner.slide_processing.dzsave_h5 import dzsave_h5
+
+test_slide_ndpi_path = "/media/hdd3/neo/test_slide_2.ndpi"
+save_dir = "/media/hdd3/neo/test_slide_2_dzsave"
+h5_path_256 = "test_slide_2_256.h5"
+h5_path_512 = "test_slide_2_512.h5"
+h5_path_1024 = "test_slide_2_1024.h5"
+
+print("Dzsaving at tile size 256")
+dzsave_h5(
+    wsi_path=test_slide_ndpi_path,
+    h5_path=os.path.join(save_dir, h5_path_256),
+    tile_size=256,
+    num_cpus=32,
+    region_cropping_batch_size=256,
+)
+
+print("Dzsaving at tile size 512")
+dzsave_h5(
+    wsi_path=test_slide_ndpi_path,
+    h5_path=os.path.join(save_dir, h5_path_512),
+    tile_size=512,
+    num_cpus=32,
+    region_cropping_batch_size=256,
+)
+
+print("Dzsaving at tile size 1024")
+dzsave_h5(
+    wsi_path=test_slide_ndpi_path,
+    h5_path=os.path.join(save_dir, h5_path_1024),
+    tile_size=1024,
+    num_cpus=32,
+    region_cropping_batch_size=256,
+)
---
+++ services/minimal_service.py
@@ -0,0 +1,10 @@
+
+# the CUTOFFDATE should be December 13th, 2024, at 2:00 PM Eastern Time
+CUTOFFDATETIME = "2024-12-13 14:00:00"
+
+LLBMA_results_dir = "/media/hdd2/neo/SameDayLLBMAResults"
+dzsave_dir = "/media/hdd2/neo/SameDayDzsave"
+metdata_path = "/media/hdd2/neo/SameDayDzsave/same_day_processing_metadata.csv"
+
+# the same_day_processing_metadata.csv should have the following columns
+# wsi_name, result_dir_name, datetime_processed, pipeline, specimen_type, datetime_dzsaved, dzsave_error, pipeline_error
