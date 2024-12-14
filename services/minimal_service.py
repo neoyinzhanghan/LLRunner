1706047988edda1dee0969d5e706e030d461c681
Neo Yin <yinzhang@berkeley.edu>
2024-12-13 16:15:00 -0800

    service set up

--- services/minimal_service.py
+++ services/minimal_service.py
@@ -1,4 +1,7 @@
 import os
+import time
+import shutil
+import datetime
 import pandas as pd
 
 cutoffdatetime = "2024-12-10 00:00:00"
@@ -13,7 +16,7 @@ dzsave_dir = "/media/hdd2/neo/SameDayDzsave"
 metdata_path = "/media/hdd2/neo/SameDayDzsave/same_day_processing_metadata.csv"
 
 # the same_day_processing_metadata.csv should have the following columns
-# wsi_name, result_dir_name, datetime_processed, pipeline, specimen_type, datetime_dzsaved, dzsave_error, pipeline_error
+# wsi_name, result_dir_name, datetime_processed, pipeline, specimen_type, datetime_dzsaved, dzsave_error, pipeline_error, slide_copy_time, dzsave_time, pipeline_time
 
 metadata_df = pd.read_csv(metdata_path)
 
@@ -52,3 +55,32 @@ for slide_name in all_slide_names:
         newer_slides.append(slide_name)
 
 print(f"Found a total of {len(newer_slides)} slides newer than the cutoff datetime.")
+
+def process_slide(slide_name):
+    # first copy the slide to the tmp_slide_dir
+    slide_path = os.path.join(slide_source_dir, slide_name)
+    tmp_slide_path = os.path.join(tmp_slide_dir, slide_name)
+
+    new_metadata_row_dict = {
+        "wsi_name": slide_name,
+        "result_dir_name": None,
+        "datetime_processed": None,
+        "pipeline": None,
+        "specimen_type": None,
+        "datetime_dzsaved": None,
+        "dzsave_error": None,
+        "pipeline_error": None,
+        "slide_copy_time": None,
+        "dzsave_time": None,
+        "pipeline_time": None,
+    }
+
+    print(f"Copying slide from {slide_name} to {tmp_slide_path}")
+    copy_start_time = time.time()
+    shutil.copy(slide_path, tmp_slide_path)
+    slide_copy_time = time.time() - copy_start_time
+    new_metadata_row_dict["slide_copy_time"] = slide_copy_time
+    print(f"Slide copy completed. Took {slide_copy_time} seconds.")
+
+    print(new_metadata_row_dict)
+
