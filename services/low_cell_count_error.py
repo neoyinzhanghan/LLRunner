3a59748751be27bce7d03af7884ce98b1dcfd210
Neo Yin <yinzhang@berkeley.edu>
2024-12-21 13:37:26 -0800

    low num cells error processing

--- services/low_cell_count_error.py
+++ services/low_cell_count_error.py
@@ -1,21 +1,54 @@
+import os
 from get_service_summary import get_number_of_regions_and_cells
 
-test_result_dir = "/media/hdd2/neo/test_slide_result_dir"
 
-(
-    num_focus_regions_passed,
-    num_unannotated_focus_regions,
-    num_cells_passed,
-    num_removed_cells,
-) = get_number_of_regions_and_cells(test_result_dir)
+def has_less_than_N_cells(result_dir_path, N=200):
+    (
+        num_focus_regions_passed,
+        num_unannotated_focus_regions,
+        num_cells_passed,
+        num_removed_cells,
+    ) = get_number_of_regions_and_cells(result_dir_path)
 
-print(f"Number of focus regions passed: {num_focus_regions_passed}")
-print(f"Number of unannotated focus regions: {num_unannotated_focus_regions}")
-print(f"Number of cells passed: {num_cells_passed}")
-print(f"Number of cells removed: {num_removed_cells}")
+    if num_cells_passed < N:
+        return True
+    return
 
-# if the number of cells passed is less than 200, print "ERROR: Number of cells passed is less than 200"
-if num_cells_passed < 200:
-    print("ERROR: Number of cells passed is less than 200. :(")
-else:
-    print("Number of cells passed is greater than 200. :)")
+
+if __name__ == "__main__":
+
+    test_result_dir = "/media/hdd2/neo/test_slide_result_dir"
+
+    if has_less_than_N_cells(test_result_dir):
+        print("Number of cells passed is less than 200. :(")
+    else:
+        print("Number of cells passed is greater than 200. :)")
+
+    root_dir = "/media/hdd2/neo/SameDayLLBMAResults"
+
+    # find the list of all subdirectories in the root_dir
+    subdirs = [
+        subdir
+        for subdir in os.listdir(root_dir)
+        if os.path.isdir(os.path.join(root_dir, subdir))
+    ]
+
+    # find the subdirectories that do not start with ERROR_
+    non_error_subdirs = [
+        subdir for subdir in subdirs if not subdir.startswith("ERROR_")
+    ]
+
+    total_non_error_subdirs = len(non_error_subdirs)
+    total_low_cells = 0
+
+    for non_error_subdir in non_error_subdirs:
+        result_dir_path = os.path.join(root_dir, non_error_subdir)
+
+        if has_less_than_N_cells(result_dir_path):
+            print(f"{result_dir_path} has less than 200 cells.")
+            total_low_cells += 1
+        else:
+            print(f"{result_dir_path} has more than 200 cells.")
+
+    print(f"Total number of subdirectories with less than 200 cells: {total_low_cells}")
+    print(f"Total number of subdirectories: {total_non_error_subdirs}")
