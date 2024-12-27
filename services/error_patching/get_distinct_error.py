803f8de5e054017bebc7afd87f82057d918ba73d
Neo Yin <yinzhang@berkeley.edu>
2024-12-27 00:03:10 -0800

    error info

--- services/error_patching/get_distinct_error.py
+++ services/error_patching/get_distinct_error.py
@@ -1,7 +1,9 @@
 import os
+import pandas as pd
 from tqdm import tqdm
 
 LLBMA_results_dir = "/media/hdd2/neo/SameDayLLBMAResults"
+LLBMA_error_mapping_path = "/media/hdd2/neo/error_mapping.csv"
 
 # find the paths to all the subdirectories in the LLBMA_results_dir that start with ERROR_
 error_subdirs = [
@@ -31,3 +33,24 @@ print(f"Number of distinct error messages: {len(error_names)}")
 print("Distinct error messages:")
 for error_name in error_names:
     print(error_name)
+
+error_mapping_df_dict = {"result_dir_name": [], "error_message": []}
+
+for error_subdir in tqdm(error_subdirs, desc="Creating error mapping"):
+    # open the error.txt file in the error_subdir as a string
+    error_txt_path = os.path.join(LLBMA_results_dir, error_subdir, "error.txt")
+
+    with open(error_txt_path, "r") as f:
+        error_message = f.read()
+
+    if (
+        error_message not in error_names
+        and "Too few focus regions found" not in error_message
+        and "Too few candidates found" not in error_message
+    ):
+        error_mapping_df_dict["result_dir_name"].append(error_subdir)
+        error_mapping_df_dict["error_message"].append(error_message)
+
+error_mapping_df = pd.DataFrame(error_mapping_df_dict)
+
+error_mapping_df.to_csv(LLBMA_error_mapping_path, index=False)
