7673ac900110fb5e92b8875a2de45b9e3fdd7320
Neo Yin <yinzhang@berkeley.edu>
2024-12-27 16:54:54 -0800

    local memory error grepping

--- services/error_patching/local_storage_error.py
+++ services/error_patching/local_storage_error.py
@@ -40,72 +40,73 @@ for subdir in tqdm(subdirs, desc="Finding error directories"):
 
 print(f"Found {len(specific_errors_dir)} directories with the specified error message")
 
-# open the slide metadata file
-metadata_df = pd.read_csv(metadata_path)
-num_metadata_rows_before_deletion = len(metadata_df)
 
-num_result_dirs_deleted = 0
+# # open the slide metadata file
+# metadata_df = pd.read_csv(metadata_path)
+# num_metadata_rows_before_deletion = len(metadata_df)
 
-for subdir in tqdm(specific_errors_dir, desc="Deleting h5 dzsave files"):
-    dzsave_h5_name = subdir + ".h5"
-    dzsave_h5_name = dzsave_h5_name.replace("ERROR_", "")
-    dzsave_h5_path = os.path.join(dzsave_dir, dzsave_h5_name)
+# num_result_dirs_deleted = 0
 
-    if os.path.exists(dzsave_h5_path):
-        os.remove(dzsave_h5_path)
-    else:
-        print(f"{dzsave_h5_path} does not exist")
-        raise FileNotFoundError(f"{dzsave_h5_path} does not exist")
+# for subdir in tqdm(specific_errors_dir, desc="Deleting h5 dzsave files"):
+#     dzsave_h5_name = subdir + ".h5"
+#     dzsave_h5_name = dzsave_h5_name.replace("ERROR_", "")
+#     dzsave_h5_path = os.path.join(dzsave_dir, dzsave_h5_name)
+
+#     if os.path.exists(dzsave_h5_path):
+#         os.remove(dzsave_h5_path)
+#     else:
+#         print(f"{dzsave_h5_path} does not exist")
+#         raise FileNotFoundError(f"{dzsave_h5_path} does not exist")
 
 
-# first delete all the directories with the specified error message
-for subdir in tqdm(specific_errors_dir, desc="Deleting error directories"):
-    result_dir_path = os.path.join(LLBMA_results_dir, subdir)
+# # first delete all the directories with the specified error message
+# for subdir in tqdm(specific_errors_dir, desc="Deleting error directories"):
+#     result_dir_path = os.path.join(LLBMA_results_dir, subdir)
 
-    if os.path.exists(result_dir_path):
-        shutil.rmtree(result_dir_path)
-    num_result_dirs_deleted += 1
+#     if os.path.exists(result_dir_path):
+#         shutil.rmtree(result_dir_path)
+#     num_result_dirs_deleted += 1
 
-    wsi_name = subdir.replace("ERROR_", "") + ".ndpi"
+#     wsi_name = subdir.replace("ERROR_", "") + ".ndpi"
 
-    # remove the subdir from the metadata_df
-    metadata_df = metadata_df[metadata_df["wsi_name"] != wsi_name]
+#     # remove the subdir from the metadata_df
+#     metadata_df = metadata_df[metadata_df["wsi_name"] != wsi_name]
 
-print(f"Found {len(specific_errors_dir)} directories with the specified error message")
-print(f"Deleted {num_result_dirs_deleted} directories")
-print(
-    f"Number of rows in metadata before deletion: {num_metadata_rows_before_deletion - len(metadata_df)}"
-)
+# print(f"Found {len(specific_errors_dir)} directories with the specified error message")
+# print(f"Deleted {num_result_dirs_deleted} directories")
+# print(
+#     f"Number of rows in metadata before deletion: {num_metadata_rows_before_deletion - len(metadata_df)}"
+# )
 
-num_error_with_no_dir = 0
+# num_error_with_no_dir = 0
 
-wsi_names_to_remove = []
+# wsi_names_to_remove = []
 
-# iterate through rows of metadata_df
-for i, row in tqdm(metadata_df.iterrows(), desc="Checking metadata"):
-    wsi_name = row["wsi_name"]
+# # iterate through rows of metadata_df
+# for i, row in tqdm(metadata_df.iterrows(), desc="Checking metadata"):
+#     wsi_name = row["wsi_name"]
 
-    error_dir_name = f"ERROR_{wsi_name.split('.ndpi')[0]}"
-    error_dir_path = os.path.join(LLBMA_results_dir, error_dir_name)
+#     error_dir_name = f"ERROR_{wsi_name.split('.ndpi')[0]}"
+#     error_dir_path = os.path.join(LLBMA_results_dir, error_dir_name)
 
-    non_error_dir_name = wsi_name.split(".ndpi")[0]
-    non_error_dir_path = os.path.join(LLBMA_results_dir, non_error_dir_name)
+#     non_error_dir_name = wsi_name.split(".ndpi")[0]
+#     non_error_dir_path = os.path.join(LLBMA_results_dir, non_error_dir_name)
 
-    is_bma = row["is_bma"]
-    is_bma = bool(is_bma)
+#     is_bma = row["is_bma"]
+#     is_bma = bool(is_bma)
 
-    if (
-        not os.path.exists(error_dir_path)
-        and not os.path.exists(non_error_dir_path)
-        and is_bma
-    ):
-        print(f"{error_dir_path} does not exist")
-        num_error_with_no_dir += 1
+#     if (
+#         not os.path.exists(error_dir_path)
+#         and not os.path.exists(non_error_dir_path)
+#         and is_bma
+#     ):
+#         print(f"{error_dir_path} does not exist")
+#         num_error_with_no_dir += 1
 
-        wsi_names_to_remove.append(wsi_name)
+#         wsi_names_to_remove.append(wsi_name)
 
-print(f"Found {num_error_with_no_dir} errors with no corresponding directory")
+# print(f"Found {num_error_with_no_dir} errors with no corresponding directory")
 
-# remove the rows from metadata_df
-metadata_df = metadata_df[~metadata_df["wsi_name"].isin(wsi_names_to_remove)]
-metadata_df.to_csv(metadata_path, index=False)
+# # remove the rows from metadata_df
+# metadata_df = metadata_df[~metadata_df["wsi_name"].isin(wsi_names_to_remove)]
+# metadata_df.to_csv(metadata_path, index=False)
