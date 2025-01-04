b92f7a24957d833e09545098b3b46f3c70b8b085
Neo Yin <yinzhang@berkeley.edu>
2025-01-03 19:45:02 -0800

    service update

--- services/delete_results.py
+++ services/delete_results.py
@@ -4,6 +4,12 @@ from tqdm import tqdm
 
 cutoffdatetime = "2025-01-03 00:00:00"
 metadata_path = "/media/hdd2/neo/same_day_processing_metadata.csv"
+slide_source_dir = "/pesgisipth/NDPI"
+tmp_slide_dir = "/media/hdd2/neo/tmp_slides_dir"
+LLBMA_results_dir = "/media/hdd2/neo/SameDayLLBMAResults"
+dzsave_dir = "/media/hdd2/neo/SameDayDzsave"
+metadata_path = "/media/hdd2/neo/same_day_processing_metadata.csv"
+topview_save_dir = "/media/hdd2/neo/tmp_slides_dir/topview"
 
 
 def get_slide_datetime(slide_name):
@@ -33,5 +39,36 @@ for index, row in tqdm(metadata.iterrows(), total=metadata.shape[0]):
     if slide_datetime > pd.to_datetime(cutoffdatetime):
         wsi_names_after_cutoff.append(slide_name)
 
+    h5_name = slide_name.replace(".ndpi", ".h5")
+    h5_path = os.path.join(dzsave_dir, h5_name)
+
+    tmp_slide_path = os.path.join(tmp_slide_dir, slide_name)
+
+    result_folder_path = os.path.join(LLBMA_results_dir, slide_name.split(".ndpi")[0])
+    error_result_folder_path = os.path.join(
+        LLBMA_results_dir, "ERROR_" + slide_name.split(".ndpi")[0]
+    )
+
+    if os.path.exists(h5_path):
+        print(f"Removing {h5_path}")
+        os.remove(h5_path)
+
+    if os.path.exists(tmp_slide_path):
+        print(f"Removing {tmp_slide_path}")
+        os.remove(tmp_slide_path)
+
+    if os.path.exists(result_folder_path):
+        print(f"Removing {result_folder_path}")
+        os.rmdir(result_folder_path)
+
+    if os.path.exists(error_result_folder_path):
+        print(f"Removing {error_result_folder_path}")
+        os.rmdir(error_result_folder_path)
 
 print(f"Found {len(wsi_names_after_cutoff)} slides after {cutoffdatetime}")
+
+# delete all the rows in the metadata that have a wsi_name in wsi_names_after_cutoff
+metadata = metadata[~metadata["wsi_name"].isin(wsi_names_after_cutoff)]
+
+# save the metadata to the same path
+metadata.to_csv(metadata_path, index=False)
