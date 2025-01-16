6d6d19b8edd849c34dbbf3595303516bc5388bae
neoyinzhanghan <yinzhang@berkeley.edu>
2025-01-15 18:27:05 -0800

    test diff data existence

--- services/test_normal_slides_diff.py
+++ services/test_normal_slides_diff.py
@@ -2,12 +2,23 @@ import os
 from get_copath_data import get_path_data, get_diff
 
 normal_slides_dir = "/media/hdd2/neo/BMA_Normal_lite"
-normal_slides_names = [slide_name for slide_name in os.listdir(normal_slides_dir) if slide_name.endswith(".ndpi")]
+normal_slides_names = [
+    slide_name
+    for slide_name in os.listdir(normal_slides_dir)
+    if slide_name.endswith(".ndpi")
+]
+
+print(normal_slides_names)
 
 accession_numbers = [slide_name.split(";")[0] for slide_name in normal_slides_names]
 
+print(accession_numbers)
+
+
+print("Getting copath data")
 copath_data = get_path_data(accession_numbers)
 
+print("Getting diff data")
 diff_data = get_diff(copath_data)
 
 print(f"Number of slides: {len(normal_slides_names)}")
